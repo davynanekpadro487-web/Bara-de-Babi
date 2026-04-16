25da6f6b942870b18c720678c11158780628232b
unknown <davynanekpadro487@gmail.com>
2026-04-17 00:17:46 +0200

    Correction bugs onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'onboarding_screen.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-=======
 import 'package:flutter/services.dart';
 import 'core/theme.dart';
 import 'screens/splash_screen.dart';
+import 'onboarding_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,29 +19,18 @@ void main() {
 
 class BaraDeBabiApp extends StatelessWidget {
   const BaraDeBabiApp({super.key});
->>>>>>> 75fc9922d75a8d1b774714443b10c26292692c2e
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
-      title: 'Artisan App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF1E88E5),
-          primary: const Color(0xFF1E88E5),
-          secondary: const Color(0xFFFF6D00),
-        ),
-        useMaterial3: true,
-      ),
-      home: const OnboardingScreen(),
-=======
       title: 'Barra de Babi',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.darkTheme,
-      home: const SplashScreen(),
->>>>>>> 75fc9922d75a8d1b774714443b10c26292692c2e
+      theme: AppTheme.darkTheme, // Ou AppTheme.lightTheme selon le besoin
+      
+      // J'ai mis l'OnboardingScreen en page d'accueil pour que vous puissiez voir  
+      // immédiatement les résultats de notre travail. Si vous voulez repasser par 
+      // le SplashScreen de votre nouveau code, remplacez simplement par : const SplashScreen()
+      home: const OnboardingScreen(),
     );
   }
 }
